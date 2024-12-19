c6876fa467e742eec243ec760331dd5e6ea1b12b
Monique Duarte <moniquead95@gmail.com>
2024-12-19 16:14:24 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    //quando clicar em "nova imagem", aparece o evento de exibição de display
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+
+    //quando clicar em "cancelar", o display é recolhido
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+
+    //quando clicar em "adicionar", a pagina não atualiza
+    $('form').on('submit', function(e) {
+        e.preventDeFault();
+        const urlAdd = $('#url-img').val ();
+        const novoItem = $('<li style ="display:none"></li>');
+        $(`<img src="${urlAdd}" />`).appendTo(novoItem);
+
+        $(` <div class = "overlay-img-link"> 
+            <a href = "${urlAdd}" targer="_blank" title= "Ver imagem e tamanho real">
+            Ver imagem em tamanho real </a> 
+            </div>`).appendTo(novoItem);
+            
+
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#url-img').val('')
+    })
+})
