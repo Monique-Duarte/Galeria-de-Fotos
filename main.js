30eace5dadbdfcf4edc0ba1f39d309021e32e1f9
Monique Duarte <moniquead95@gmail.com>
2024-12-30 09:21:29 -0300

    commit add.

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 $(document).ready(function(){
-    //quando clicar em "nova imagem", aparece o evento de exibição de display
     $('header button').click(function(){
         $('form').slideDown();
     })
 
-    
-    //quando clicar em "cancelar", o display é recolhido
-    $('#botao-cancelar').click(function() {
+    $('#botao-cancelar').click(function(){
         $('form').slideUp();
     })
 
 
-    //quando clicar em "adicionar", a pagina não atualiza
-    $('form').on('submit', function(e) {
-        e.preventDeFault();
-        const urlAdd = $('#url-img').val ();
-        const novoItem = $('<li style ="display:none"></li>');
-        $(`<img src="${urlAdd}" />`).appendTo(novoItem);
+    $('form').on('submit',function(e){
+        e.preventDefault();
 
-        $(` <div class = "overlay-img-link"> 
-            <a href = "${urlAdd}" targer="_blank" title= "Ver imagem e tamanho real">
-            Ver imagem em tamanho real </a> 
-            </div>`).appendTo(novoItem);
-            
+    const enderecoDaNovaImagem = $('#url-imagem-nova').val();
+    const novoItem =$('<li style= display:none></li>');
+    $(`<img src = "${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+    
+    $(`<div class = "overlay-imagem-link">
+    <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+    Ver imagem em tamanho real
+    </a>
+    </div>
+    `).appendTo(novoItem);
 
         $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(1000);
-        $('#url-img').val('')
+        $('#url-imagem-nova').val('')
     })
 })
